561819797c42e643e469fa6029c3396d13fa3646
Jeremy Kim <thk116@gmail.com>
2019-03-28 01:54:30 -0700

    cleaned up code for button text event

--- script.js
+++ script.js
@@ -11,18 +11,20 @@ let lightSwitch = function() {
 */
 let lightSwitch = function() {
     console.log("bang")
-    document.querySelector("#switch").addEventListener("click", () => {
+    let switchButton = document.querySelector("#switch")
+    
+    switchButton.addEventListener("click", () => {
         let light = document.getElementsByClassName("night")
-        let text = document.getElementById("switch").textContent
+        let text = switchButton.textContent
 
         for(let item of light) {
             item.classList.toggle("light")
         }
 
         if(text === "Lights On") {
-            document.getElementById("switch").textContent = "Lights Off"
+            switchButton.textContent = "Lights Off"
         } else {
-            document.getElementById("switch").textContent = "Lights On"
+            switchButton.textContent = "Lights On"
         }
     })
 }
